d8933cbae3c5a575ae36a4dc0eec59ceeceaaed8
Shahzain Ahmed <shahzainahmed57@gmail.com>
2026-08-27 22:12:07 +0500

    Remove onboarding preference logic and simplify app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,18 @@ import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:mobile_app_with_api/app/constants/app_themes.dart';
-import 'package:mobile_app_with_api/app/modules/onboarding/onboarding_screen.dart';
 import 'package:mobile_app_with_api/app/routes/app_pages.dart';
 import 'package:mobile_app_with_api/app/routes/app_routes.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  final preferences = await SharedPreferences.getInstance();
-  final hasSeenOnboarding =
-      preferences.getBool(OnboardingScreen.preferenceKey) ?? false;
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.bottom]);
-  runApp(CarRentalApp(showOnboarding: !hasSeenOnboarding));
+  runApp(CarRentalApp());
 }
 
 class CarRentalApp extends StatelessWidget {
-  const CarRentalApp({super.key, this.showOnboarding = true});
-
-  final bool showOnboarding;
+  const CarRentalApp({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +26,7 @@ class CarRentalApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           title: 'Car Rental App',
           theme: AppThemes.primaryTheme,
-          initialRoute: showOnboarding ? AppRoutes.onboarding : AppRoutes.home,
+          initialRoute: AppRoutes.onboarding,
           getPages: AppPages.pages,
         );
       },
